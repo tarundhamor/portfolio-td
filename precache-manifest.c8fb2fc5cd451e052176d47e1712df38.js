5c98eed58bbe71273785d22e4bacaae23b15d93b
tarundhamor <tarundhamor@gmail.com>
2020-05-03 14:50:30 +0530

    Updates

---
+++ precache-manifest.c8fb2fc5cd451e052176d47e1712df38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45119f41a13e36f0d48846c3773704f",
+    "url": "/td-v2-portfolio/index.html"
+  },
+  {
+    "revision": "3c8f6bbc449e38592fee",
+    "url": "/td-v2-portfolio/static/css/main.37ad7eb3.chunk.css"
+  },
+  {
+    "revision": "566447482aa703049791",
+    "url": "/td-v2-portfolio/static/js/2.e64004f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/td-v2-portfolio/static/js/2.e64004f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8f6bbc449e38592fee",
+    "url": "/td-v2-portfolio/static/js/main.56859d14.chunk.js"
+  },
+  {
+    "revision": "bbafc42b75fe05039129",
+    "url": "/td-v2-portfolio/static/js/runtime-main.3813ae7f.js"
+  },
+  {
+    "revision": "d315cfbe18c146ad893dead03f27c169",
+    "url": "/td-v2-portfolio/static/media/darkPic.d315cfbe.jpg"
+  },
+  {
+    "revision": "d315cfbe18c146ad893dead03f27c169",
+    "url": "/td-v2-portfolio/static/media/lightPic.d315cfbe.jpg"
+  }
+]);
