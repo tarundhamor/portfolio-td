0928edc6ab6e214c7bd35bab68ee9b927411e7bb
tarundhamor <tarundhamor@gmail.com>
2020-07-15 12:53:43 +0530

    Updated Projects

--- src/Containers/Portfolio/data.js
+++ src/Containers/Portfolio/data.js
@@ -5,16 +5,19 @@ export default [
 		name: "Image Steganography",
 		logo: "https://i.imgur.com/jiU8kZF.png",
 		description:
-			"This is a small project where Image Steganography is implemented using LSB method using Javascript and to showcase it's working HTML, CSS is used. This can be used with avatar generator so user can store password in avatar image for a particular password. Encryption techniques can also be used to make it more secure.",
-		lang:
-			"Javascript - NodeJS - HTML - CSS",
-		images: [
-			"https://i.imgur.com/D0RieeF.png"
-			
-		],
-		url: "https://github.com/tarundhamor"
-	}
+			"In this project Image Steganography is implemented using LSB method using Javascript and to showcase it's working HTML, CSS is used. This can be used with avatar generator so user can store password in avatar image for a particular password. Encryption techniques can also be used to make it more secure.",
+		lang: "Javascript - NodeJS - HTML - CSS",
+		images: ["https://i.imgur.com/D0RieeF.png"],
+		url: "https://github.com/tarundhamor",
+	},
 
-	
-	
+	{
+		name: "Search-o-Fast",
+		logo: "https://i.imgur.com/TBw4RIs.png",
+		description:
+			"Trie is an efficient information reTrieval data structure. Using Trie, search complexities can be brought to optimal limit (key length). In this project a search implementation is made and also compared with Linear Search. The Trie search returns the search queries and suggestions much faster than the Linear Search. A list of 10,000 words is populated in the tree and then upon searching suggesions are being given for similar words.",
+		lang: "Javascript - NodeJS - HTML - CSS - Trie",
+		images: ["https://i.imgur.com/6EcPEN6.png"],
+		url: "https://github.com/tarundhamor",
+	},
 ]
